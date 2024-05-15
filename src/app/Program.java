319cd38088045319d6b283b3c7948f85032043f3
Rafaelgome2 <rafael3rsg@gmai.com>
2024-05-15 14:44:57 -0300

    department entity class

---
+++ src/app/Program.java
@@ -0,0 +1,13 @@
+package app;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department dep1 = new Department(1, "soraia");
+		System.out.println(dep1);
+
+	}
+
+}
